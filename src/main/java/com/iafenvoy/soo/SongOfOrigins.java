1a75c5c50fe350f27a8c037d2bcd65c2fe904ac3
IAFEnvoy <tanksheng545@126.com>
2024-09-12 14:47:37 +0800

    init

---
+++ src/main/java/com/iafenvoy/soo/SongOfOrigins.java
@@ -0,0 +1,12 @@
+package com.iafenvoy.soo;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SongOfOrigins implements ModInitializer {
+    public static final String MOD_ID = "songs_of_origins";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/iafenvoy/soo/client/SongOfOriginsClient.java
@@ -0,0 +1,10 @@
+package com.iafenvoy.soo.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SongOfOriginsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
