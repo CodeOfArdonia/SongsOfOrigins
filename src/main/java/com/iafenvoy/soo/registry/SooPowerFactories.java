5ed69fc9e7e88717172395cea60a344fa95fc19b
IAFEnvoy <tanksheng545@126.com>
2025-07-17 11:44:04 +0800

    add action

--- src/main/java/com/iafenvoy/soo/registry/SooPowerFactories.java
+++ src/main/java/com/iafenvoy/soo/registry/SooPowerFactories.java
@@ -4,7 +4,6 @@ import com.iafenvoy.soo.power.SongPowerPower;
 import com.iafenvoy.sow.SongsOfWar;
 import io.github.apace100.apoli.power.Power;
 import io.github.apace100.apoli.power.factory.PowerFactory;
-import io.github.apace100.apoli.power.factory.PowerFactorySupplier;
 import io.github.apace100.apoli.registry.ApoliRegistries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -16,9 +15,5 @@ public class SooPowerFactories {
 
     private static void register(PowerFactory<?> powerFactory) {
         Registry.register(ApoliRegistries.POWER_FACTORY, powerFactory.getSerializerId(), powerFactory);
-    }
-
-    private static void register(PowerFactorySupplier<?> factorySupplier) {
-        register(factorySupplier.createFactory());
     }
 }
